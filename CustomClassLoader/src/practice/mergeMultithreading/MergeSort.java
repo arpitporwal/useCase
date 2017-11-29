72d75e856c32bef6c8c75ce21623e2d62cb56a35
Arpit Porwal <appiporwal@gmail.com>
2017-11-29 10:42:37 +0530

    a

--- CustomClassLoader/src/practice/mergeMultithreading/MergeSort.java
+++ CustomClassLoader/src/practice/mergeMultithreading/MergeSort.java
@@ -10,29 +10,102 @@ public class MergeSort
 		int arr[] = {12, 11, 13, 5, 6, 7};
 		int left = 0;
 		int right = arr.length;
+		int mid = (left + right) / 2;
 		
-		
-		while(left < right)
+		Thread t = new Thread(new Runnable(){
+			@Override
+			public void run()
+			{
+				CountDownLatch latch = new CountDownLatch(2);
+				MergeSort.divide(arr, left, mid, latch);
+				MergeSort.divide(arr, mid+1, right, latch);
+			}
+		});
+				
+		t.start();
+	}
+	
+	private static void divide(int[] arr, int start, int end, CountDownLatch latch)
+	{
+		if(start < end)
 		{
-			CountDownLatch latch = new CountDownLatch(2);
-			int mid = (left + right)/ 2;
+			int mid = (start + end) / 2;
+			Thread t1 = new Thread(new Runnable(){
+				@Override
+				public void run()
+				{
+					CountDownLatch latch = new CountDownLatch(2);
+					MergeSort.divide(arr, start, mid, latch);
+				}
+			});
+			t1.start();
 			
-			MergeService mergeService1 = new MergeService(arr, left, mid-1);
-			MergeService mergeService2 = new MergeService(arr, mid, right);
-			
-			SortThread st1 = new SortThread(mergeService1, latch);
-			SortThread st2 = new SortThread(mergeService2, latch);
-			
+			Thread t2 = new Thread(new Runnable(){
+				@Override
+				public void run()
+				{
+					CountDownLatch latch = new CountDownLatch(2);
+					MergeSort.divide(arr, mid+1, end, latch);
+				}
+			});
+			t2.start();
 			try
 			{
+				
 				latch.await();
 			}
-			catch (InterruptedException e)
+			catch(InterruptedException e)
 			{
 				e.printStackTrace();
 			}
-			
-			
 		}
 	}
+	
+	public void merge(int[] arr, int left, int mid, int right)
+	{
+		int lSize = mid - left + 1;
+		int rSize = right - mid;
+		
+		int lArray[] = new int[lSize];
+		for(int i=0; i<lSize; i++)
+		{
+			lArray[i] = arr[left + i];
+		}
+		
+		int rArray[] = new int[rSize];
+		for(int i=0; i<rSize; i++)
+		{
+			rArray[i] = arr[mid + i + 1];
+		}
+		
+		int i=0,j=0,k=left;
+		
+		while(i<lSize && j<rSize)
+		{
+			if(lArray[i] <= rArray[j])
+			{
+				arr[k] = lArray[i];
+				i++;
+			}
+			else
+			{
+				arr[k] = rArray[j];
+				j++;
+			}
+			k++;
+		}
+		
+		while(i<lSize)
+		{
+			arr[k] = lArray[i];
+			i++; k++;
+		}
+		
+		while(j<rSize)
+		{
+			arr[k] = rArray[j];
+			j++; k++;
+		}
+	}
+	
 }
